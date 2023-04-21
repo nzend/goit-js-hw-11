1270e169f24261cb4a25aa4672cdf677c754ff2f
nzend <115723110+nzend@users.noreply.github.com>
2023-04-21 18:57:14 +0300

    Add firs version

--- src/fetch-cards.js
+++ src/fetch-cards.js
@@ -1,11 +1,31 @@
-export default function fetchCards(searchQuery) {
-  let page = 1;
-  const axios = require('axios');
-  const URl = ``;
-  const options = {
-    key: '',
-  };
-  return fetch(URl, options).then(responce => {
-    responce.json();
-  });
+import axios from 'axios';
+const options = {
+  baseUrl: 'https://pixabay.com/api/',
+  key: '35630597-0391cfe0949186f41cbd4414f',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  order: 'popular',
+  page: 1,
+  per_page: '4',
+};
+
+export default async function fetchCards(searchImg) {
+  options.page += 1;
+  const {
+    baseUrl,
+    key,
+    image_type,
+    orientation,
+    safesearch,
+    order,
+    page,
+    per_page,
+  } = options;
+
+  const response = await axios.get(
+    `${baseUrl}?key=${key}&q=${searchImg}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&order=${order}&page=${page}&per_page=${per_page}`
+  );
+  const results = response.data;
+  return results;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,105 @@
 import './css/styles.css';
 import fetchCards from './fetch-cards';
+import { debounce } from 'debounce';
+import Notiflix from 'notiflix';
 
-const axios = require('axios');
+console.log(fetchCards);
+const DEBOUNCE_DELAY = 1000;
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  searchBtn: document.querySelector('.search-form button'),
+  input: document.querySelector('.search-form input'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+let searchImg = '';
+// let page = 1;
+
+refs.input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+refs.form.addEventListener('submit', onSearchBtn);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+refs.searchBtn.disabled = true;
+
+function onLoadMoreBtn(event) {
+  event.preventDefault();
+
+  fetchCards(searchImg).then(response => {
+    if (response.total === 0) {
+      clearData();
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    cardsMurkup(response);
+  });
+}
+
+function onSearchBtn(event) {
+  event.preventDefault();
+  refs.searchBtn.disabled = false;
+
+  fetchCards(searchImg).then(response => {
+    clearData();
+    if (response.total === 0) {
+      clearData();
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      refs.loadMoreBtn.classList.toggle('is-visible');
+    }
+    refs.searchBtn.disabled = true;
+
+    refs.loadMoreBtn.classList.toggle('is-visible');
+    cardsMurkup(response);
+  });
+}
+
+function onInputChange() {
+  searchImg = refs.input.value.trim();
+  if (searchImg === '') {
+    refs.loadMoreBtn.classList.remove('is-visible');
+
+    clearData();
+    refs.searchBtn.disabled = true;
+
+    Notiflix.Notify.info('Введіть для пошуку');
+    return;
+  }
+  refs.searchBtn.disabled = false;
+}
+
+function cardsMurkup({ hits, total }) {
+  const cards = hits
+    .map(
+      card =>
+        // console.log(card);
+        `
+    <div class="photo-card">
+        <img src="${card.webformatURL}" alt="" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b> ${card.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${card.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${card.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${card.downloads}
+          </p>
+        </div>
+      </div>`
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', cards);
+}
+
+function clearData() {
+  refs.gallery.innerHTML = '';
+  refs.gallery.innerHTML = '';
+}
